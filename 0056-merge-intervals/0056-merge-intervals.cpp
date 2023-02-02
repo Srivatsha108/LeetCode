ffccd740e60e72c96d977db62aa937a078f91317
V V SRIVATSHA <55289346+Srivatsha108@users.noreply.github.com>
2023-02-02 16:22:54 +0530

    Time: 42 ms (70.15%), Space: 19.2 MB (50.06%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,40 +1,40 @@
 class Solution {
 public:
-   static bool comp(vector<int>m1,vector<int>m2)
-    {
-        return m1[0]<=m2[0];
-    }
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(),intervals.end());
+   // static bool comp(vector<int>m1,vector<int>m2)
+   //  {
+   //      return m1[0]<=m2[0];
+   //  }
+    vector<vector<int>> merge(vector<vector<int>>& in) {
+        sort(in.begin(),in.end());
         
-        // vector<vector<int>>res;
-        // vector<vector<int>>r(1,in[0]);
-        // for(int i=1;i<in.size();i++)
-        // {
-        //     if((r[0][0]>=in[i][0] && r[0][0]<=in[i][1]) || (in[i][0]>=r[0][0] && in[i][0]<=r[0][1]))
-        //         r[0][0]=min(r[0][0],in[i][0]),r[0][1]=max(r[0][1],in[i][1]);
-        //     else{
-        //         res.push_back(r[0]);
-        //         r[0][0]=in[i][0];
-        //         r[0][1]=in[i][1];
-        //     }
-        // }
-        // res.push_back(r[0]);
-        // return res;
-        // 
-        vector<vector<int>> merged;
-        for (auto interval : intervals) {
-            // if the list of merged intervals is empty or if the current
-            // interval does not overlap with the previous, simply append it.
-            if (merged.empty() || merged.back()[1] < interval[0]) {
-                merged.push_back(interval);
-            }
-            // otherwise, there is overlap, so we merge the current and previous
-            // intervals.
-            else {
-                merged.back()[1] = max(merged.back()[1], interval[1]);
+        vector<vector<int>>res;
+        vector<vector<int>>r(1,in[0]);
+        for(int i=1;i<in.size();i++)
+        {
+            if((r[0][0]>=in[i][0] && r[0][0]<=in[i][1]) || (in[i][0]>=r[0][0] && in[i][0]<=r[0][1]))
+                r[0][0]=min(r[0][0],in[i][0]),r[0][1]=max(r[0][1],in[i][1]);
+            else{
+                res.push_back(r[0]);
+                r[0][0]=in[i][0];
+                r[0][1]=in[i][1];
             }
         }
-        return merged;
+        res.push_back(r[0]);
+        return res;
+        
+        // vector<vector<int>> merged;
+        // for (auto interval : intervals) {
+        //     // if the list of merged intervals is empty or if the current
+        //     // interval does not overlap with the previous, simply append it.
+        //     if (merged.empty() || merged.back()[1] < interval[0]) {
+        //         merged.push_back(interval);
+        //     }
+        //     // otherwise, there is overlap, so we merge the current and previous
+        //     // intervals.
+        //     else {
+        //         merged.back()[1] = max(merged.back()[1], interval[1]);
+        //     }
+        // }
+        // return merged;
     }
 };
