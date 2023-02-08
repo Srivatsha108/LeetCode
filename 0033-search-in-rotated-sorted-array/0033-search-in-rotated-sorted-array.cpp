e55b0439f2646c0eb83bd3b3bf8c4c4ce12303ea
V V SRIVATSHA <55289346+Srivatsha108@users.noreply.github.com>
2023-02-09 00:35:49 +0530

    Time: 3 ms (83.47%), Space: 11.1 MB (41.98%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int search(vector<int>& a, int target) {
+        // int l=0,h=a.size()-1;
+        // while(l<h)
+        // {
+        //     int m=(l+h)/2;
+        //     if(a[m]==target)
+        //         return m;
+        //     if(m>1 && a[m]>a[m-1] && a[m]<a[m+1])
+        //     {
+        //         if(target<a[m])
+        //             h=m-1;
+        //         else
+        //             l=m+1;
+        //     }
+        //     else
+        //     {
+        //         if(target<a[m] && target<=a[a.size()-1])
+        //             l=m+1;
+        //         else if(target>a[m] && target<=a[a.size()-1])
+        //         else
+        //             h=m-1;
+        //     }
+        // }
+        // return a[l]==target?l:-1;
+        int l=0,h=a.size()-1;
+        while(l<=h)
+        {
+            int m=(l+h)/2;
+            if(a[m]==target)
+                return m;
+            if(a[m]>=a[l])
+            {
+                
+                if(target>=a[l] && target<=a[m])
+                {
+                   h=m-1;
+                }
+                else
+                    l=m+1;
+            
+            }
+            else
+            {
+                if(target>=a[m] && target<=a[h])
+                    l=m+1;
+                else
+                    h=m-1;
+            }
+            
+        }
+        return -1;
+    }
+};
