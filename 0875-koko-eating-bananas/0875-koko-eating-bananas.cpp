23ab67923552bae8699cbbd7d300471d3db1c94d
V V SRIVATSHA <55289346+Srivatsha108@users.noreply.github.com>
2023-02-13 00:48:24 +0530

    Time: 44 ms (85.30%), Space: 19 MB (42.95%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,34 +1,19 @@
 class Solution {
 public:
-    int minEatingSpeed(vector<int>& piles, int H) {
-        // int r=0,h=*max_element(piles.begin(),piles.end()),l=1;
-        // while(l<=h)
-        // {
-        //     int m=l+(h-l)/2,r1=0;
-        //     for(auto i:piles)
-        //     {
-        //         r1+=(i/m+(i%m)?1:0);
-        //     }
-        //     // if(r==0)
-        //     //     r=r1;
-        //     // if(r1<=h1)
-        //     //     r=min(r,r1),h=m-1;
-        //     if(r1>=h1)
-        //         l=m+1;
-        //     else
-        //         h=m-1;
-        // }
-        // return r;
-        int l = 1, r = 1000000000;
-        while (l < r) {
-            int m = (l + r) / 2, total = 0;
-            for (int p : piles)
-                total += (p + m - 1) / m;
-            if (total > H)
-                l = m + 1;
-            else
-                r = m;
+    int minEatingSpeed(vector<int>& piles, int h1) {
+        long h=*max_element(piles.begin(),piles.end()),l=1,r=h;
+        while(l<=h)
+        {
+            long m=l+(h-l)/2,r1=0;
+            for(auto i:piles)
+            {
+                r1+=(i/m+((i%m)?1:0));
+            }
+            if(r1<=h1)
+                r=min(r,m),h=m-1;
+            else l=m+1;
         }
-        return l;
+        return r;
+        
     }
 };
