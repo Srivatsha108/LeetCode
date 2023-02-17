a40c82649e3188ce7ef7874740045600f1e4f31b
V V SRIVATSHA <55289346+Srivatsha108@users.noreply.github.com>
2023-02-17 15:58:35 +0530

    Time: 8 ms (79.44%), Space: 7.2 MB (85.13%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        if(s.length()!=t.length())
+            return false;
      int c[26]={0};
-     for(auto i:s)
-         c[i-97]++;
-    for(auto i:t)
-        c[i-97]--;
+     for(int i=0;i<s.length();i++)
+         c[s[i]-97]++,c[t[i]-97]--;
+    
     for(auto i:c)
         if(i!=0)
             return false;
