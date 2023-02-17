9fba011b99293d3486bec23efddc0dc3ba4c78fd
V V SRIVATSHA <55289346+Srivatsha108@users.noreply.github.com>
2023-02-17 12:41:32 +0530

    Time: 2 ms (69.56%), Space: 10 MB (6.62%) - LeetHub

--- 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.cpp
+++ 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.cpp
@@ -11,25 +11,43 @@
  */
 class Solution {
 public:
-    vector<int>res;
+    void find(TreeNode *root,vector<int>&res)
+    {
+         if(root==NULL)
+            return;
+        find(root->left,res);
+        res.push_back(root->val);
+        find(root->right,res);
+    }
+  
     int minDiffInBST(TreeNode* root) {
-        queue<TreeNode*>q;
-        q.push(root);
-        while(!q.empty())
-        {
-            TreeNode *t=q.front();
-            q.pop();
-            res.push_back(t->val);
-            if(t->left)
-                q.push(t->left);
-            if(t->right)
-                q.push(t->right);
-        }
-        sort(res.begin(),res.end());
+        // queue<TreeNode*>q;
+        // q.push(root);
+        // while(!q.empty())
+        // {
+        //     TreeNode *t=q.front();
+        //     q.pop();
+        //     res.push_back(t->val);
+        //     if(t->left)
+        //         q.push(t->left);
+        //     if(t->right)
+        //         q.push(t->right);
+        // }
+        // sort(res.begin(),res.end());
+        // int r=INT_MAX;
+        // for(int i=0;i<res.size()-1;i++)
+        //     if(res[i+1]-res[i]<r)
+        //         r=res[i+1]-res[i];
+        // return r;
+          vector<int>res;
+        find(root,res);
         int r=INT_MAX;
         for(int i=0;i<res.size()-1;i++)
             if(res[i+1]-res[i]<r)
                 r=res[i+1]-res[i];
         return r;
+        
     }
+    
+    
 };
