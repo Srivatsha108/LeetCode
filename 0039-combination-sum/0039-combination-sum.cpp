1b6b8f5cfacea392c5849967accb12545d9ec067
V V SRIVATSHA <55289346+Srivatsha108@users.noreply.github.com>
2023-02-24 00:15:18 +0530

    Time: 7 ms (70.45%), Space: 10.8 MB (60.61%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,32 +1,54 @@
 class Solution {
 public:
-    void find(vector<int>&can,int target,int s,vector<vector<int>>&v,int i,vector<int>&temp)
+    // void find(vector<int>&can,int target,int s,vector<vector<int>>&v,int i,vector<int>&temp)
+    // {
+    //     if(s>target)
+    //         return;
+    //     if(s==target)
+    //     {
+    //         v.push_back(temp);
+    //         return;
+    //     }
+    //     // temp.push_back(can[i]);
+    //     // s+=can[i];
+    //     // find(can,target,s,v,i,temp);
+    //     // s-=can[i];
+    //     // temp.pop_back();
+    //     for(;i<can.size();i++)
+    //     {
+    //         temp.push_back(can[i]);
+    //         s+=can[i];
+    //         find(can,target,s,v,i,temp);
+    //         s-=can[i];
+    //         temp.pop_back();
+    //     }
+    // }
+    void find(vector<int>&can,int target,vector<vector<int>>&v,vector<int>&temp,int i)
     {
-        if(s>target)
-            return;
-        if(s==target)
+        if(i==can.size())
         {
-            v.push_back(temp);
-            return;
+            if(target==0)
+            {
+                v.push_back(temp);
+                return;
+            }
+            else
+                return;
         }
-        // temp.push_back(can[i]);
-        // s+=can[i];
-        // find(can,target,s,v,i,temp);
-        // s-=can[i];
-        // temp.pop_back();
-        for(;i<can.size();i++)
+        if(can[i]<=target)
         {
             temp.push_back(can[i]);
-            s+=can[i];
-            find(can,target,s,v,i,temp);
-            s-=can[i];
+            find(can,target-can[i],v,temp,i);
             temp.pop_back();
         }
+        // temp.pop_back();
+        find(can,target,v,temp,i+1);
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
      vector<vector<int>>v;
         vector<int>temp;
-     find(candidates,target,0,v,0,temp);
+     // find(candidates,target,0,v,0,temp);
+        find(candidates,target,v,temp,0);
         return v;
     }
 };
