6a2a2ce044d87374b52ff7cb186bdb9c71fb8c6f
V V SRIVATSHA <55289346+Srivatsha108@users.noreply.github.com>
2023-02-24 19:40:41 +0530

    Time: 5 ms (72.05%), Space: 10.4 MB (96.92%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+//     void fin(vector<int>&can,int target,int i,int s,vector<int>temp,vector<vector<int>>&res)
+//     {
+//         if(i==can.size())
+//         {
+//             if(s==target)
+//             {
+//                 sort(temp.begin(),temp.end());
+//                 if(find(res.begin(),res.end(),temp)==res.end())
+//                     res.push_back(temp);
+//             }  
+//             return;
+//         }
+//         else
+//             return;
+//         for(int j=i;j<can.size();j++)
+//         {
+//             s+=can[j];
+//             temp.push_back(can[j]);
+//             fin(can,target,i+1,s,temp,res);
+//             s-=can[j];
+//             temp.pop_back();
+//         }
+//     }
+//     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+//         vector<int>temp;
+//         vector<vector<int>>res;
+//         fin(candidates,target,0,0,temp,res);
+//         return res;
+//     }
+// };
+    vector<vector<int> > combinationSum2(vector<int> &num, int target) 
+    {
+        vector<vector<int>> res;
+        sort(num.begin(),num.end());
+        vector<int> local;
+        findCombination(res, 0, target, local, num);
+        return res;
+    }
+    void findCombination(vector<vector<int>>& res, const int order, const int target, vector<int>& local, const vector<int>& num)
+    {
+        if(target==0)
+        {
+            res.push_back(local);
+            return;
+        }
+        else
+        {
+            for(int i = order;i<num.size();i++) // iterative component
+            {
+                if(num[i]>target) return;
+                if(i&&num[i]==num[i-1]&&i>order) continue; // check duplicate combination
+                local.push_back(num[i]),
+                findCombination(res,i+1,target-num[i],local,num); // recursive componenet
+                local.pop_back();
+            }
+        }
+    }
+};
