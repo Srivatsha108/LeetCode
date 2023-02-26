c448a7c1ddba198aa2197d79e6bd8610a208d735
V V SRIVATSHA <55289346+Srivatsha108@users.noreply.github.com>
2023-02-26 12:15:56 +0530

    Time: 0 ms (100.00%), Space: 10.7 MB (43.31%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,35 +1,59 @@
 class Solution {
 public:
-//     void fin(vector<int>&can,int target,int i,int s,vector<int>temp,vector<vector<int>>&res)
+    // void fin(vector<int>&can,int target,int i,int s,vector<int>temp,vector<vector<int>>&res)
+    // {
+    //     if(i==can.size())
+    //     {
+    //         if(s==target)
+    //         {
+    //             sort(temp.begin(),temp.end());
+    //             if(find(res.begin(),res.end(),temp)==res.end())
+    //                 res.push_back(temp);
+    //         }  
+    //         return;
+    //     }
+    //     else
+    //         return;
+    //     for(int j=i;j<can.size();j++)
+    //     {
+    //         s+=can[j];
+    //         temp.push_back(can[j]);
+    //         fin(can,target,i+1,s,temp,res);
+    //         s-=can[j];
+    //         temp.pop_back();
+    //     }
+    // }
+//     void fin(vector<int>&can,int target,vector<int>temp,vector<vector<int>>&res,int i)
 //     {
 //         if(i==can.size())
 //         {
-//             if(s==target)
+//             if(target==0)
 //             {
 //                 sort(temp.begin(),temp.end());
 //                 if(find(res.begin(),res.end(),temp)==res.end())
 //                     res.push_back(temp);
-//             }  
+//                 return;
+//             }
 //             return;
 //         }
-//         else
-//             return;
-//         for(int j=i;j<can.size();j++)
-//         {
-//             s+=can[j];
-//             temp.push_back(can[j]);
-//             fin(can,target,i+1,s,temp,res);
-//             s-=can[j];
+//         if(can[i]<=target){
+//             temp.push_back(can[i]);
+//             fin(can,target-can[i],temp,res,i+1);
 //             temp.pop_back();
-//         }
+            
+//             }
+//         fin(can,target,temp,res,i+1);
+        
+        
 //     }
-//     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-//         vector<int>temp;
-//         vector<vector<int>>res;
-//         fin(candidates,target,0,0,temp,res);
-//         return res;
-//     }
-// };
+    
+    // vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+    //     vector<int>temp;
+    //     vector<vector<int>>res;
+    //     findCombination(candidates,target,temp,res,0);
+    //     return res;
+    // }
+
     vector<vector<int> > combinationSum2(vector<int> &num, int target) 
     {
         vector<vector<int>> res;
