f04f947140041800a1fc4175cfca36895b6970ac
V V SRIVATSHA <55289346+Srivatsha108@users.noreply.github.com>
2023-03-08 23:13:14 +0530

    Time: 9 ms (43.04%), Space: 14.3 MB (14.57%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,84 +1,27 @@
 class Solution {
 public:
-    // void fin(vector<int>&can,int target,int i,int s,vector<int>temp,vector<vector<int>>&res)
-    // {
-    //     if(i==can.size())
-    //     {
-    //         if(s==target)
-    //         {
-    //             sort(temp.begin(),temp.end());
-    //             if(find(res.begin(),res.end(),temp)==res.end())
-    //                 res.push_back(temp);
-    //         }  
-    //         return;
-    //     }
-    //     else
-    //         return;
-    //     for(int j=i;j<can.size();j++)
-    //     {
-    //         s+=can[j];
-    //         temp.push_back(can[j]);
-    //         fin(can,target,i+1,s,temp,res);
-    //         s-=can[j];
-    //         temp.pop_back();
-    //     }
-    // }
-//     void fin(vector<int>&can,int target,vector<int>temp,vector<vector<int>>&res,int i)
-//     {
-//         if(i==can.size())
-//         {
-//             if(target==0)
-//             {
-//                 sort(temp.begin(),temp.end());
-//                 if(find(res.begin(),res.end(),temp)==res.end())
-//                     res.push_back(temp);
-//                 return;
-//             }
-//             return;
-//         }
-//         if(can[i]<=target){
-//             temp.push_back(can[i]);
-//             fin(can,target-can[i],temp,res,i+1);
-//             temp.pop_back();
-            
-//             }
-//         fin(can,target,temp,res,i+1);
-        
-        
-//     }
-    
-    // vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-    //     vector<int>temp;
-    //     vector<vector<int>>res;
-    //     findCombination(candidates,target,temp,res,0);
-    //     return res;
-    // }
-
-    vector<vector<int> > combinationSum2(vector<int> &num, int target) 
-    {
-        vector<vector<int>> res;
-        sort(num.begin(),num.end());
-        vector<int> local;
-        findCombination(res, 0, target, local, num);
-        return res;
-    }
-    void findCombination(vector<vector<int>>& res, const int order, const int target, vector<int>& local, const vector<int>& num)
+    void solve(int i,vector<int>ds,vector<vector<int>>&res,int target,vector<int>can)
     {
         if(target==0)
         {
-            res.push_back(local);
-            return;
+            res.push_back(ds);
         }
-        else
+        for(int j=i;j<can.size();j++)
         {
-            for(int i = order;i<num.size();i++) // iterative component
-            {
-                if(num[i]>target) return;
-                if(i&&num[i]==num[i-1]&&i>order) continue; // check duplicate combination
-                local.push_back(num[i]),
-                findCombination(res,i+1,target-num[i],local,num); // recursive componenet
-                local.pop_back();
-            }
+            if(can[j]>target)
+                break;
+            if(j && can[j]==can[j-1] && j>i)
+                continue;
+            ds.push_back(can[j]);
+            solve(j+1,ds,res,target-can[j],can);
+            ds.pop_back();
         }
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+        vector<int>ds;
+        vector<vector<int>>res;
+        solve(0,ds,res,target,candidates);
+        return res;
     }
 };
