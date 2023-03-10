a31ad28d4ba08eecf9567f4ed7849748b0730a9d
V V SRIVATSHA <55289346+Srivatsha108@users.noreply.github.com>
2023-03-10 20:23:07 +0530

    Time: 4 ms (35.64%), Space: 6.5 MB (53.02%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,5 +1,14 @@
 class Solution {
 public:
+    vector<string>solve(vector<string>res,string s)
+    {
+        vector<string>temp;
+        for(auto i:res)
+            for(auto j:s)
+                temp.push_back(i+j);
+        // res=temp;
+        return temp;
+    }
     vector<string> letterCombinations(string digits) {
         vector<string>res;
         if(digits.empty())
@@ -7,13 +16,18 @@ public:
         vector<string>v{"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
         
         res.push_back("");
-        for(auto digit:digits)
+//         for(auto digit:digits)
+//         {
+//             vector<string>temp;
+//             for(auto j:v[digit-'0'])
+//                 for(auto s:res)
+//                     temp.push_back(s+j);
+//             swap(res,temp);
+//         }
+//         return res;
+        for(auto i:digits)
         {
-            vector<string>temp;
-            for(auto j:v[digit-'0'])
-                for(auto s:res)
-                    temp.push_back(s+j);
-            swap(res,temp);
+            res=solve(res,v[i-'0']);
         }
         return res;
     }
